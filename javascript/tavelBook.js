51544b2d6620b9de52ad1da1f541601def6734d9
kreisa <kreisa@gnosis>
2015-10-07 01:59:58 +0800

    add style to map

--- javascript/tavelBook.js
+++ javascript/tavelBook.js
@@ -1,20 +1,47 @@
+var map = (function() {
+    
+    var travelledMap;
+    
+    return {
+        init: init
+    };
+    
+    function init() {
+        travelledMap = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: 13.0000, lng: 122.0000},
+            zoom: 5,
+            scrollwheel: false,
+            styles: getStyle()
+        }); 
+    }
+    
+    function styleWrapper(featureType, elementType, stylers) {
+        return {
+            featureType : featureType,
+            elementType : elementType,
+            stylers     : stylers
+        }
+    }
+    
+    function getStyle() {
+        return [ 
+                styleWrapper("landscape.natural", "geometry.fill", [{ "visibility":"on","color": "#e0efef" }]),
+                styleWrapper("poi", "geometry.fill", [{"visibility": "on"},{"hue": "#1900ff"},{"color": "#c0e8e8"}]),
+                styleWrapper("road", "geometry", [{"lightness": 100},{"visibility": "simplified"}]),
+                styleWrapper("road", "labels", [{"visibility": "off"}]),
+                styleWrapper("transit.line", "geometry", [{"visibility": "on", "lightness": 700}]),
+                styleWrapper("water", "all", [{"color": "#7dcdcd"}])                
+            ];
+    }
+})();
+
 var travelBook = (function(){
     return {
         init: init
     };
     
-    var map;
-    
     function init() {
-        console.log('initializing stuff');
-        initMap();
-    }
-    
-    function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: 13.0000, lng: 122.0000},
-            zoom: 5
-        });
+        map.init();
     }
     
 })();
