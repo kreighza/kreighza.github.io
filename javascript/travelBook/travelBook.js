69d08eaca8d9626295cf674e808f6d69b9438445
kreisa <kreisa@gnosis>
2016-02-20 04:16:02 +0800

    add visited places.

--- javascript/travelBook/travelBook.js
+++ javascript/travelBook/travelBook.js
@@ -1,3 +1,37 @@
 $(function(){
-    jQuery('#map').vectorMap({ map: 'world_en' });
+
+    init();
+
+    function init() {
+
+        var placesVisited = initPlacesVisited();
+        var totalPlacesVisited = Object.keys(placesVisited).length;
+
+        jQuery('#map').vectorMap({
+            map: 'world_en',
+            colors: placesVisited,
+            hoverColor: null,
+            onRegionClick: onRegionClick
+        });
+
+        $('div.places-visited-total#trivia1').html(totalPlacesVisited);
+        $('span.places-visited-total').html(totalPlacesVisited);
+    }
+
+    function onRegionClick(event, code, region) {
+        event.preventDefault();
+    }
+
+    function initPlacesVisited() {
+        var placesVisited = {};
+        var blue = '#628FC4';
+
+        placesVisited['ph'] = blue;
+        placesVisited['th'] = blue;
+        placesVisited['vn'] = blue;
+        placesVisited['my'] = blue;
+
+        return placesVisited;
+    }
+
 });
