cc51f57645059cb293027eacd58fd170f6d6ba47
kreisa <kreisa@gnosis>
2016-02-20 04:20:43 +0800

    Site updated at 2016-02-19 20:20:43 UTC

--- javascript/travelBook/travelBook.js
+++ javascript/travelBook/travelBook.js
@@ -1,71 +1,37 @@
-var map = (function() {
-    
-    var travelledMap;
-    var visitedPlaces;
-    var geocoder;
-    
-    return {
-        init: init
-    };
-    
-    function init(places) {
-        geocoder = new google.maps.Geocoder();
-        travelledMap = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: 13.0000, lng: 122.0000},
-            zoom: 5,
-            scrollwheel: false,
-            mapTypeControl: false,
-            streetViewControl: false,
-            styles: getStyle()
-        }); 
-                
-        places.forEach(function(place) {
-            markOnMap(place);
-        });
-    }
-    
-    function styleWrapper(featureType, elementType, stylers) {
-        return {
-            featureType : featureType,
-            elementType : elementType,
-            stylers     : stylers
-        }
-    }
-    
-    function getStyle() {
-        return [ 
-                styleWrapper('landscape.natural', 'geometry.fill', [{ 'visibility':'off'}]),
-                styleWrapper('poi', 'geometry.fill', [{'visibility': 'on'},{'hue': '#1900ff'},{'color': '#c0e8e8'}]),
-                styleWrapper('road', 'geometry', [{'lightness': 100},{'visibility': 'simplified'}]),
-                styleWrapper('road', 'labels', [{'visibility': 'off'}]),
-                styleWrapper('transit.line', 'geometry', [{'visibility': 'on', 'lightness': 700}]),
-                styleWrapper('water', 'all', [{'color': '#7dcdcd'}])                
-            ];
-    }
-    
-    function markOnMap(place) {
-        geocoder.geocode({'address': place.name}, function(results, status) {
-            if (status === google.maps.GeocoderStatus.OK) {
-                var marker = new google.maps.Marker({
-                    position: results[0].geometry.location,
-                    map: travelledMap,
-                    title: place.name,
-                    icon: '../images/travelBook/mapMarker.png'
-                });
-            } else {
-                console.error('Cannot find coordinates for ' + place.name + ' because: ' + status);
-            }
-        });
-    }
-})();
+$(function(){
 
-var travelBook = (function(){
-    return {
-        init: init
-    };
-    
+    init();
+
     function init() {
-        visitedPlaces.init();
-        map.init(visitedPlaces.getAllPlaces());
+
+        var placesVisited = initPlacesVisited();
+        var totalPlacesVisited = Object.keys(placesVisited).length;
+
+        jQuery('#map').vectorMap({
+            map: 'world_en',
+            colors: placesVisited,
+            hoverColor: null,
+            onRegionClick: onRegionClick
+        });
+
+        $('div.places-visited-total#trivia1').html(totalPlacesVisited);
+        $('span.places-visited-total').html(totalPlacesVisited);
     }
-})();
+
+    function onRegionClick(event, code, region) {
+        event.preventDefault();
+    }
+
+    function initPlacesVisited() {
+        var placesVisited = {};
+        var blue = '#628FC4';
+
+        placesVisited['ph'] = blue;
+        placesVisited['th'] = blue;
+        placesVisited['vn'] = blue;
+        placesVisited['my'] = blue;
+
+        return placesVisited;
+    }
+
+});
